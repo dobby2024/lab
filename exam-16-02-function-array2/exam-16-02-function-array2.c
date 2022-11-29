4e113166616e32c0f1381d6ebad57c04d907e8d9
ITSC <ITSC@gamil.com>
2022-11-29 21:46:03 +0900

    11/29

---
+++ exam-16-02-function-array2/exam-16-02-function-array2.c
@@ -0,0 +1,32 @@
+/*
+파일명 : exam-16-02-function-array2
+*/
+#include <stdio.h>
+int readArray(int arr[], int length)
+{
+	int i;
+	
+	printf("배열의 요소 읽어보기 : [ ");
+	for (i = 0; i < length; i++)
+	{
+		printf("%d", arr[i]);
+		if (i + 1 < length)
+		{
+			printf(", ");
+		}
+		else
+		{
+			printf(" ");
+		}
+	}
+	printf("]\n");
+}
+
+int main(void)
+{
+	int arr[3] = { 3, 6, 9 };
+	int length = sizeof(arr) / sizeof(int);
+	readArray(arr, length); // 배열의 이름 arr를 포인터 변수에 대입
+
+	return 0;
+}
